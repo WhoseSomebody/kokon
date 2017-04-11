fcbce0d0f49893ac6befd66c207bb06d05ee4b0c
WhoseSomebody <whosesomebody@gmail.com>
2017-04-11 13:17:08 +0300

    https 1

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const stylus = require('express-stylus');
 const path = require('path');
 const nib = require('nib');
 const app = express();
+const https = require('https');
 const publicDir = path.join(__dirname, '/public');
+const options = {
+   key  : fs.readFileSync('server.key'),
+   cert : fs.readFileSync('server.crt')
+};
 
 app.set('views', __dirname + '/public/views');
 app.set("view engine", "pug");
@@ -15,11 +20,11 @@ app.use(stylus({
     import: ['nib']
 }));
 
-app.use(express.static(publicDir));
+app.use(express.static(publicDir, {maxAge: 604800000 }));
 app.use('/', routes);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+https.createServer(options, app).listen(port, () => {
     console.log("Listening on " + port);
 });
 
