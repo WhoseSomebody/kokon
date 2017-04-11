8f723298f220ce71ed2ab5b17e5f49fa93b043d5
WhoseSomebody <whosesomebody@gmail.com>
2017-04-11 15:09:47 +0300

    optimizing robots.txt

--- app.js
+++ app.js
@@ -23,9 +23,20 @@ app.use(stylus({
 }));
 app.use(express.static(publicDir, {maxAge: 604800000 }));
 app.use(minify());
-app.get('http://*',function(req,res){  
-    res.redirect('https://kokon.co.ua'+req.url)
-})
+var https_redirect = function () {
+  return function(req, res, next) {
+    if (req.secure) {
+      if(env === 'development') {
+        return res.redirect('https://localhost:3000' + req.url);
+      } else {
+        return res.redirect('https://' + req.headers.host + req.url);
+      }
+    } else {
+      return next();
+    }
+  };
+};
+app.use(https_redirect());
 app.use('/', routes);
 
 const port = process.env.PORT || 3000;
