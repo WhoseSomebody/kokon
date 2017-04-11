72a6c65cbb7a014b7829fac68b56182da30340ce
WhoseSomebody <whosesomebody@gmail.com>
2017-04-11 13:22:21 +0300

    https 2

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const fs = require('fs');
+// const fs = require('fs');
 const routes = require('./routes/index');
 const stylus = require('express-stylus');
 const path = require('path');
 const nib = require('nib');
 const app = express();
-const https = require('https');
+// const https = require('https');
 const publicDir = path.join(__dirname, '/public');
-const options = {
-   key  : fs.readFileSync('server.key'),
-   cert : fs.readFileSync('server.crt')
-};
+// const options = {
+//    key  : fs.readFileSync('server.key'),
+//    cert : fs.readFileSync('server.crt')
+// };
 
 app.set('views', __dirname + '/public/views');
 app.set("view engine", "pug");
@@ -25,7 +25,7 @@ app.use(express.static(publicDir, {maxAge: 604800000 }));
 app.use('/', routes);
 
 const port = process.env.PORT || 3000;
-https.createServer(options, app).listen(port, () => {
+app.listen(port, () => {
     console.log("Listening on " + port);
 });
 
